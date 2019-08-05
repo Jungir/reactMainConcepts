7b25d0026584929adf75c6586109d27279ff24ec
Jungir <googlman6300@gmail.com>
2019-08-05 18:15:12 +0600

    creare-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first react app</h1>
+      <p>Welcome</p>
+    </div>
+  );
+}
+
+export default App;
