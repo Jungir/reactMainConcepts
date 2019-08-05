280b9319cdd7edaef3c122024cc99eb405724409
Jungir <googlman6300@gmail.com>
2019-08-05 18:25:25 +0600

    nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
+      
       <h1>My first react app</h1>
       <p>Welcome</p>
+      <Ninjas/>
     </div>
   );
 }
