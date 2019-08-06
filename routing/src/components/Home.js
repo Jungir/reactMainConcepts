eaba06148145b82e352f67ad3b8524f902ce263d
Jungir <googlman6300@gmail.com>
2019-08-06 20:43:40 +0600

    fetching data in compDidMount+axios

--- routing/src/components/Home.js
+++ routing/src/components/Home.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
-const Home = () =>{
-    return (
+import React, {Component} from 'react'
+import axios from 'axios';
+class Home extends Component {
+    state = {
+        posts : []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {console.log(res);
+                this.setState({
+                posts : res.data.slice(0, 10)
+            });
+        }).catch(err => console.log(err));
+    }
+    render(){
+        const {posts} = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                </div>)
+            })
+        ) : (<div className="center">No posts yet</div>);
+        return (
         <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis accusantium dolorem sit iusto exercitationem possimus voluptate, reprehenderit nam! Quos dolorem tempore quis voluptas architecto soluta praesentium natus quia asperiores.</p>
-        </div>
-    )
+            <h4>Home</h4>
+            {postList}
+        </div>)
+    }
 }
 export default Home;
