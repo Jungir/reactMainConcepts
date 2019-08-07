6b42aec008edb99c1691c8fac349add2ff331746
Jungir <googlman6300@gmail.com>
2019-08-07 12:10:57 +0600

    mapping piece of state to props

--- routing/src/reducers/rootRecucer.js
+++ routing/src/reducers/rootRecucer.js
@@ -1,5 +1,9 @@
 const initalState = {
-    posts : []
+    posts : [
+        {id: 1, title: 'The best cheif ever', body: ' If you want your app to work offline and load faster'},
+        {id: 2, title: 'The best cheif ever 2', body: ' If you want your app to work offline and load faster'},
+        {id: 3, title: 'The best cheif ever 3', body: ' If you want your app to work offline and load faster'}
+    ]
 };
 const rootReducer = (state = initalState, action)=>{
     return state;
